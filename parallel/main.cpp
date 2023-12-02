2b2c61265693a7658296e46272ae1e66db0fd3eb
ndaruvala <ndaruvala2346@gmail.com>
2023-12-02 16:07:18 -0500

    Add tiling, buggy image

--- parallel/main.cpp
+++ parallel/main.cpp
@@ -5,32 +5,42 @@ int main() {
     ispc::Camera camera;
     camera.aspectRatio = 16.0f / 9.0f;
     camera.imageWidth = 400;
-    camera.samplesPerPixel = 64;
-    camera.maxDepth = 15;
+    camera.samplesPerPixel = 100;
+    camera.maxDepth = 50;
     ispc::initialize(camera);
 
     ispc::Sphere spheres[2];
     
-    spheres[0].center = {0, 0, -1};
+    ispc::float3 center1;
+    center1.v[0] = 0;
+    center1.v[1] = 0;
+    center1.v[2] = -1;
+
+    spheres[0].center = center1;
     spheres[0].radius = 0.5;
-    
-    spheres[1].center = {0, -100.5, -1};
+
+    ispc::float3 center2;
+    center2.v[0] = 0;
+    center2.v[1] = -100.5;
+    center2.v[2] = -1;
+
+    spheres[1].center = center2;
     spheres[1].radius = 100;
 
     int* out = new int[camera.imageWidth * camera.imageHeight * 3];
 
-
     ispc::renderPixel(camera, spheres, 2, out);
 
     std::cout << "P3\n" << camera.imageWidth << ' ' << camera.imageHeight << "\n255\n";
-    for (int j = 0; j < camera.imageHeight; j++) {
-        for (int i = 0; i < camera.imageWidth; i++) {
-            int index = (j * camera.imageWidth + i) * 3;
-            std::cout << out[index] << ' ' << out[index + 1] << ' ' << out[index + 2] << '\n';
+    for (int j = 0; j < camera.imageHeight; ++j) {
+        std::clog << "\rScanlines remaining: " << (camera.imageHeight - j) << ' ' << std::flush;
+        for (int i = 0; i < camera.imageWidth; ++i) {
+            int k = (j * camera.imageWidth + i) * 3;
+            std::cout << out[k] << ' ' << out[k + 1] << ' ' << out[k + 2] << '\n';
         }
     }
+    std::clog << "\rDone.                 \n";
 
     delete[] out;
-    
     return 0;
 }
